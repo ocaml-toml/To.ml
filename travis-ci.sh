e86869cbfba3fb797c88a86cdde86082e1a53388
Thomas Wickham <thomas@wickham.epimeros.org>
2014-01-18 17:46:27 +0100

    Add travis support (3)

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,21 +5,31 @@ export OPAM_VERSION=1.1.0
 # OPAM packages needed to build tests
 export OPAM_PACKAGES='ocamlfind ounit menhir'
 
+echo '** INSTALLING OCAML'
+
 # install ocaml from apt
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml
 
+echo '** INSTALLING OPAM'
+
 # install opam
-curl https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz | tar xz -C /tmp
+pushd /tmp
+curl https://codeload.github.com/ocaml/opam/tar.gz/${OPAM_VERSION} | tar xz /tmp/opam-${OPAM_VERSION}
 pushd /tmp/opam-${OPAM_VERSION}
-./configure
-make
-sudo make install
+./configure > /dev/null
+make 2> /dev/null
+sudo make install > /dev/null 2> /dev/null
 opam init -a
 eval `opam config -env`
 popd
+popd
+
+echo '** INSTALING DEPENDENCIES'
 
 # install packages from opam
 opam install -q -y ${OPAM_PACKAGES}
+
+echo '** TEST'
 
 make test
