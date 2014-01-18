a7245019ad20c9f3474bd69af74ba1e58aa08570
Thomas Wickham <thomas@wickham.epimeros.org>
2014-01-18 18:32:12 +0100

    Add travis CI support (7)

--- travis-ci.sh
+++ travis-ci.sh
@@ -8,8 +8,8 @@ export OPAM_PACKAGES='ocamlfind ounit menhir'
 echo '** INSTALLING OCAML'
 
 # install ocaml from apt
-sudo add-apt-repository ppa:mike-mcclurg/ocaml < `echo ''`
-sudo add-apt-repository ppa:avsm/ppa < `echo ''`
+sudo add-apt-repository ppa:mike-mcclurg/ocaml -y
+sudo add-apt-repository ppa:avsm/ppa -y
 sudo apt-get update -qq
 sudo apt-get install -qq ocaml opam
 
