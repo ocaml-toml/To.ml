a82fe6c4a7ede0e8097930585aa0115310b243fc
Julien Sagot <ju.sagot@gmail.com>
2014-02-03 21:45:20 +0100

    Fixed print (moving string_of_list from mutually recursive to out of it)

--- src/pprint.ml
+++ src/pprint.ml
@@ -1,20 +1,15 @@
 open TypeTo
 
-(**
- * FIXME: printer is broken
- * (see it uncommenting 'open Pprint' in test_toml.ml)
- *)
+let string_of_list (stringifier : 'a -> string) (els : 'a list)  =
+  String.concat "; " @@ List.map stringifier els
 
-let rec string_of_table tbl =
+let rec string_of_table tbl : string =
   Hashtbl.fold (fun k v acc -> (k, v) :: acc) tbl []
   |> string_of_list (fun (k, v) -> k ^ "->" ^ string_of_entrie v)
 
-and string_of_entrie = function
+and string_of_entrie : tomlEntrie -> string = function
   | TTable tbl -> "TTable(" ^ string_of_table tbl ^ ")"
   | TValue v -> "TValue(" ^ string_of_val v ^ ")"
-
-and string_of_list (stringifier : 'a -> string) (els : 'a list)  =
-  String.concat "; " @@ List.map stringifier els
 
 and string_of_node : tomlNodeArray -> string = function
   | NodeBool l -> string_of_list string_of_bool l
@@ -24,7 +19,7 @@ and string_of_node : tomlNodeArray -> string = function
   | NodeDate l ->  string_of_list (fun x -> x) l
   | NodeArray l ->  string_of_list string_of_node l
 
-and string_of_val = function
+and string_of_val : tomlValue -> string = function
   | TBool b -> "TBool(" ^ string_of_bool b ^ ")"
   | TInt i ->  "TInt(" ^ string_of_int i ^ ")"
   | TFloat f -> "TFloat(" ^ string_of_float f ^ ")"
