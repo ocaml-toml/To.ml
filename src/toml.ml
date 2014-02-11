0efc4423d424d505f598c71ec33f2c4918ad643a
Thomas Wickham <thomas@wickham.epimeros.org>
2014-02-12 00:17:19 +0100

    Refactoring of Toml getters, see details
    Changes applied:
    * replace `get_values` by the shorter `get`
    * replace `match x with` by the shorter `function x`
    * getters raise the Bad_Type exception to be coherent with the Hashtbl.find

--- src/toml.ml
+++ src/toml.ml
@@ -27,58 +27,58 @@ let values_to_list toml =
                 | TTable _ -> acc
                 | _ -> (k, v) :: acc) toml []
 
-(** Generic getter *)
 
-let get_value toml key = Hashtbl.find toml key
+let get = Hashtbl.find
+exception Bad_Type of (string * string)
 
 (**
  * Functions to retreive values of an expected type
  *)
 
-let get_table toml key = match Hashtbl.find toml key with
+let get_table toml key = function get toml key
   | TTable(tbl) -> tbl
-  | _ -> failwith (key ^ " is a value")
+  | _ -> raise (Bad_Type (key * "value"))
 
-let get_bool toml key = match get_value toml key with
+let get_bool toml key = function get toml key
   | TBool b -> b
-  | _ -> failwith (key ^ " is not a boolean")
+  | _ -> raise (Bad_Type (key * "boolean"))
 
-let get_int toml key = match get_value toml key with
+let get_int toml key = function get toml key
   | TInt i -> i
-  | _ -> failwith (key ^ " is not an integer")
+  | _ -> raise (Bad_Type (key * "integer"))
 
-let get_float toml key = match get_value toml key with
+let get_float toml key = function get toml key
   | TFloat f -> f
-  | _ -> failwith (key ^ " is not a float")
+  | _ -> raise (Bad_Type (key * "float"))
 
-let get_string toml key = match get_value toml key with
+let get_string toml key = function get toml key
   | TString s -> s
-  | _ -> failwith (key ^ " is not a string")
+  | _ -> raise (Bad_Type (key * "string"))
 
-let get_date toml key = match get_value toml key with
+let get_date toml key = function get toml key
   | TDate d -> d
-  | _ -> failwith (key ^ " is not a date")
+  | _ -> raise (Bad_Type (key * "date"))
 
 (**
  * Functions to retreive OCaml primitive type list
  *)
 
-let get_bool_list toml key = match get_value toml key with
+let get_bool_list toml key = function get toml key
   | TArray (NodeBool b) -> b
-  | _ -> failwith (key ^ " is not a boolean array")
+  | _ -> raise (Bad_Type (key * "boolean array"))
 
-let get_int_list toml key = match get_value toml key with
+let get_int_list toml key = function get toml key
   | TArray (NodeInt i) -> i
-  | _ -> failwith (key ^ " is not an integer array")
+  | _ -> raise (Bad_Type (key * "integer array"))
 
-let get_float_list toml key = match get_value toml key with
+let get_float_list toml key = function get toml key
   | TArray (NodeFloat f) -> f
-  | _ -> failwith (key ^ " is not a float array")
+  | _ -> raise (Bad_Type (key * "float array"))
 
-let get_string_list toml key = match get_value toml key with
+let get_string_list toml key = function get toml key
   | TArray (NodeString s) -> s
-  | _ -> failwith (key ^ " is not a string array")
+  | _ -> raise (Bad_Type (key * "string array"))
 
-let get_date_list toml key = match get_value toml key with
+let get_date_list toml key = function get toml key
   | TArray (NodeDate d) -> d
-  | _ -> failwith (key ^ " is not a date array")
+  | _ -> raise (Bad_Type (key * "date array"))
