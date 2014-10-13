fb4b0f62e189246fef17e2b411ea9225b9775145
Julien Sagot <ju.sagot@gmail.com>
2014-10-13 14:28:49 +0200

    Renamed types (removed `toml` prefix).

--- src/tomlType.mli
+++ src/tomlType.mli
@@ -1,25 +1,19 @@
-(** Toml types used by parser *)
-
-(** Toml arrays are mplemented as lists. Typed, but can contain multiple 
-    types if you nest tables *)
-type tomlNodeArray =
+type array =
   | NodeEmpty
   | NodeBool of bool list
   | NodeInt of int list
   | NodeFloat of float list
   | NodeString of string list
   | NodeDate of Unix.tm list
-  | NodeArray of tomlNodeArray list (* this can have any type *)
+  | NodeArray of array list (* this can have any type *)
 
-(** Toml primitive values *)
-and tomlValue =
+and value =
   | TBool of bool
   | TInt of int
   | TFloat of float
   | TString of string
   | TDate of Unix.tm
-  | TArray of tomlNodeArray
-  | TTable of tomlTable
+  | TArray of array
+  | TTable of table
 
-(** Toml table. Implemented as hash table. *)
-and tomlTable = (string, tomlValue) Hashtbl.t
+and table = (string * value) list
