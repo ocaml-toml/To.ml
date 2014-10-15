08410f7ce925986913457f12bde2b2f4c1c4088f
Julien Sagot <ju.sagot@gmail.com>
2014-10-15 10:27:40 +0200

    Removed [string_of_] prefix from Dump functions. Added Equal module for table/value/array equality testing.

--- src/tomlInternal.ml
+++ src/tomlInternal.ml
@@ -30,32 +30,32 @@ module Dump = struct
 
   open Type
 
-  let string_of_list (stringifier : 'a -> string) (els : 'a list)  =
+  let list (stringifier : 'a -> string) (els : 'a list)  =
     String.concat "; " @@ List.map stringifier els
 
-  let rec string_of_table (tbl : table) : string =
+  let rec table (tbl : table) : string =
     Map.fold (fun k v acc -> (k, v) :: acc) tbl []
-    |> string_of_list (fun (k, v) -> k ^ "->" ^ string_of_val v)
+    |> list (fun (k, v) -> k ^ "->" ^ value v)
 
-  and string_of_node : array -> string = function
+  and array : array -> string = function
     | NodeEmpty -> ""
-    | NodeBool l -> string_of_list string_of_bool l
-    | NodeInt l ->  string_of_list string_of_int l
-    | NodeFloat l ->  string_of_list string_of_float l
-    | NodeString l ->  string_of_list (fun x -> x) l
-    | NodeDate l ->  string_of_list string_of_date l
-    | NodeArray l ->  string_of_list string_of_node l
+    | NodeBool l -> list string_of_bool l
+    | NodeInt l ->  list string_of_int l
+    | NodeFloat l ->  list string_of_float l
+    | NodeString l ->  list (fun x -> x) l
+    | NodeDate l ->  list date l
+    | NodeArray l ->  list array l
 
-  and string_of_val : value -> string = function
+  and value : value -> string = function
     | TBool b -> "TBool(" ^ string_of_bool b ^ ")"
     | TInt i ->  "TInt(" ^ string_of_int i ^ ")"
     | TFloat f -> "TFloat(" ^ string_of_float f ^ ")"
     | TString s -> "TString(" ^ s ^ ")"
-    | TDate d -> "TDate(" ^ string_of_date d ^ ")"
-    | TArray arr -> "[" ^ string_of_node arr ^ "]"
-    | TTable tbl -> "TTable(" ^ string_of_table tbl ^ ")"
+    | TDate d -> "TDate(" ^ date d ^ ")"
+    | TArray arr -> "[" ^ array arr ^ "]"
+    | TTable tbl -> "TTable(" ^ table tbl ^ ")"
 
-  and string_of_date (d : Unix.tm) : string =
+  and date (d : Unix.tm) : string =
     "{"
     ^ string_of_int d.Unix.tm_year
     ^ "-"
@@ -64,4 +64,20 @@ module Dump = struct
     ^ string_of_int d.Unix.tm_mday
     ^ "-"
     ^ "}"
+end
+
+module Equal = struct
+
+  let rec value (x : Type.value) (y : Type.value) = match x, y with
+    | TArray x, TArray y -> array x y
+    | TTable x, TTable y -> table x y
+    | _, _               -> x = y
+
+  and array (x : Type.array) (y : Type.array) = match x, y with
+    | NodeArray x, NodeArray y -> List.for_all2 array x y
+    | _, _                     -> x = y
+
+  and table (x : Type.table) (y : Type.table) =
+    Type.Map.equal value x y
+
 end
