8753272e326a7746ea31468b3c98485e562622e2
Julien Sagot <ju.sagot@gmail.com>
2014-10-20 15:50:12 +0200

    Fixed [all_values_are_table]. Simplyfied string printing. Removed / factorized some small portions of code.

--- src/tomlPrinter.ml
+++ src/tomlPrinter.ml
@@ -2,25 +2,17 @@ open TomlInternal.Type
 
 module TomlMap = TomlInternal.Type.Map
 
-let maybe_escape_char ch =
+let maybe_escape_char formatter ch =
   match ch with
-  | '"'   -> "\\\""
-  | '\\'  -> "\\\\"
-  | '\n'  -> "\\n"
-  | '\t'  -> "\\t"
-  | _     ->
+  | '"'  -> Format.pp_print_string formatter "\\\""
+  | '\\' -> Format.pp_print_string formatter "\\\\"
+  | '\n' -> Format.pp_print_string formatter "\\n"
+  | '\t' -> Format.pp_print_string formatter "\\t"
+  | _    ->
     let code = Char.code ch in
-    match code with
-    | _ when code <= 31 -> Printf.sprintf "\\u%04x" code
-    | _                 -> String.make 1 ch
-
-let sanitize_string value =
-  let buffer = Buffer.create (String.length value) in
-  let concat_function ch =
-    maybe_escape_char ch |> Buffer.add_string buffer
-  in
-  String.iter concat_function value;
-  Buffer.contents buffer
+    if code <= 31
+    then Format.fprintf formatter "\\u%04x" code
+    else Format.pp_print_char formatter ch
 
 let print_bool formatter value = Format.pp_print_bool formatter value
 
@@ -28,7 +20,10 @@ let print_int formatter value = Format.pp_print_int formatter value
 
 let print_float formatter value = Format.pp_print_float formatter value
 
-let print_string formatter value = Format.fprintf formatter "\"%s\"" (sanitize_string value)
+let print_string formatter value =
+  Format.pp_print_char formatter '"' ;
+  String.iter (maybe_escape_char formatter) value ;
+  Format.pp_print_char formatter '"'
 
 let print_date formatter d =
   let open UnixLabels
@@ -41,9 +36,9 @@ let rec print_array formatter toml_array =
   let print_list values ~f:print_item_func =
     let pp_sep formatter () = Format.pp_print_string formatter ", "
     in
-    Format.pp_print_string formatter "[";
+    Format.pp_print_char formatter '[';
     Format.pp_print_list ~pp_sep print_item_func formatter values;
-    Format.pp_print_string formatter "]"
+    Format.pp_print_char formatter ']'
   in
   match toml_array with
   | NodeBool values   -> print_list values ~f:print_bool
@@ -54,16 +49,15 @@ let rec print_array formatter toml_array =
   | NodeArray values  -> print_list values ~f:print_array
   | NodeEmpty         -> Format.pp_print_string formatter "[]"
 
+let is_table = fun _ -> function TTable _ -> true | _ -> false
+
 let rec print_table formatter toml_table sections =
     (*
      * We need to print non-table values first, otherwise we risk including
      * top-level values in a section by accident
      *)
-  let (table_with_non_table_values, table_with_table_values) =
-    TomlMap.partition (fun _ value  ->
-        match value with
-        | TTable _  -> false
-        | _         -> true) toml_table
+  let (table_with_table_values, table_with_non_table_values) =
+    TomlMap.partition is_table toml_table
   in
   let print_key_value key value =
     print_value_with_key formatter key value sections
@@ -84,28 +78,21 @@ and print_value_with_key formatter key toml_value sections =
   let sections', add_linebreak = match toml_value with
     | TTable value  ->
       let sections_with_key = sections @ [key] in
-            (*
-             * Don't print the intermediate sections, if all values are tables,
-             * print [x.y.z] as appropriate instead of [x][y][z]
-             *)
-      let all_values_are_table =
-        TomlMap.fold (fun _ v acc ->
-            match v with
-            | TTable _  -> true
-            | _         -> false
-          ) value true
-      in
-      if not all_values_are_table then
-        Format.fprintf formatter "[%s]\n" (String.concat "." sections_with_key)
-      else ();
+      (*
+       * Don't print the intermediate sections, if all values are tables,
+       * print [x.y.z] as appropriate instead of [x][y][z]
+       *)
+      if not (TomlMap.for_all is_table value)
+      then Format.fprintf formatter "[%s]\n"
+          (String.concat "." sections_with_key) ;
       (sections_with_key, false)
     | _             ->
       Format.fprintf formatter "%s = " key;
       (sections, true)
   in
   print_value formatter toml_value sections';
-  if add_linebreak then
-    Format.fprintf formatter "\n"
+  if add_linebreak
+  then Format.pp_print_char formatter '\n'
 
 let value formatter toml_value =
   print_value formatter toml_value [];
