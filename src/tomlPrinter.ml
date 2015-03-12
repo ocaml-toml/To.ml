05668b934e50bc0df20d71d894e9e1377409b621
Julien Sagot <ju.sagot@gmail.com>
2015-03-13 00:48:24 +0100

    Printer: refactoring.

--- src/tomlPrinter.ml
+++ src/tomlPrinter.ml
@@ -1,140 +1,133 @@
 open TomlInternal.Type
 
-module TomlMap = TomlInternal.Type.Map
-module TomlKey = TomlInternal.Type.Key
+module M = TomlInternal.Type.Map
+module K = TomlInternal.Type.Key
 
-let maybe_escape_char formatter ch =
+let maybe_escape_char fmt ch =
   match ch with
-  | '"'  -> Format.pp_print_string formatter "\\\""
-  | '\\' -> Format.pp_print_string formatter "\\\\"
-  | '\n' -> Format.pp_print_string formatter "\\n"
-  | '\t' -> Format.pp_print_string formatter "\\t"
-  | _    ->
-    let code = Char.code ch in
-    if code <= 31
-    then Format.fprintf formatter "\\u%04x" code
-    else Format.pp_print_char formatter ch
+  | '"'  -> Format.pp_print_string fmt "\\\""
+  | '\\' -> Format.pp_print_string fmt "\\\\"
+  | '\n' -> Format.pp_print_string fmt "\\n"
+  | '\t' -> Format.pp_print_string fmt "\\t"
+  | _    -> let code = Char.code ch in
+            if code <= 31
+            then Format.fprintf fmt "\\u%04x" code
+            else Format.pp_print_char fmt ch
 
-let print_bool formatter value = Format.pp_print_bool formatter value
+let pp_bool fmt value = Format.pp_print_bool fmt value
 
-let print_int formatter value = Format.pp_print_int formatter value
+let pp_int fmt value = Format.pp_print_int fmt value
 
-let print_float formatter value =
-  let fractional = abs_float (value -. (floor value)) in
-  (* Even 1.'s fractional value is not equal to 0. *)
-  if fractional <= epsilon_float then
-    Format.fprintf formatter "%.1f" value
-  else
-    Format.pp_print_float formatter value
+let pp_float fmt value =
+  let (frac, int) = modf value in
+  if abs_float frac <= epsilon_float
+  then Format.fprintf fmt "%.1f" value
+  else Format.pp_print_float fmt value
 
-let print_string formatter value =
-  Format.pp_print_char formatter '"' ;
-  String.iter (maybe_escape_char formatter) value ;
-  Format.pp_print_char formatter '"'
+(* Surround string with double quote *)
+let pp_string fmt value =
+  Format.pp_print_char fmt '"' ;
+  String.iter (maybe_escape_char fmt) value ;
+  Format.pp_print_char fmt '"'
 
-let print_date fmt d =
+let pp_date fmt d =
   ISO8601.Permissive.pp_datetimezone fmt (d, 0.)
 
 (* This function is a shim for [Format.pp_print_list] from ocaml 4.02 *)
-let pp_print_list ~pp_sep print_item_func formatter values =
+let pp_list fmt pp_sep printer values =
   match values with
-  | []    -> ()
-  | e::[] -> print_item_func formatter e
-  | e::l  ->
-    print_item_func formatter e;
-    List.iter (fun v -> pp_sep formatter (); print_item_func formatter v) l
+  | []     -> ()
+  | [ e ]  -> printer fmt e
+  | e :: l -> printer fmt e;
+              List.iter (fun v -> pp_sep fmt () ;
+                                  printer fmt v) l
 
-let is_table = fun _ -> function TTable _ -> true | _ -> false
+let is_table =
+  fun _ -> function TTable _ -> true | _ -> false
 
-let is_array_of_table = fun _ -> function TArray (NodeTable _) -> true | _ -> false
+let rec pp_array fmt a sections =
 
-let rec print_array formatter toml_array sections =
-  let print_list values ~f:print_item_func =
-    let pp_sep formatter () = Format.pp_print_string formatter ", "
-    in
-    Format.pp_print_char formatter '[';
-    pp_print_list ~pp_sep print_item_func formatter values;
-    Format.pp_print_char formatter ']'
-  in
-  match toml_array with
-  | NodeBool values   -> print_list values ~f:print_bool
-  | NodeInt values    -> print_list values ~f:print_int
-  | NodeFloat values  -> print_list values ~f:print_float
-  | NodeString values -> print_list values ~f:print_string
-  | NodeDate values   -> print_list values ~f:print_date
-  | NodeArray values  ->
-      print_list values ~f:(fun formatter arr -> print_array formatter arr sections)
-  | NodeTable values  ->
-      List.iter (fun tbl ->
-        (*
-         * Don't print the intermediate sections, if all values are arrays of tables,
-         * print [[x.y.z]] as appropriate instead of [[x]][[y]][[z]]
-         *)
-        if not (TomlMap.for_all is_array_of_table tbl)
-        then Format.fprintf formatter "[[%s]]\n"
-            (sections |> List.map TomlKey.to_string |> String.concat ".");
-        print_table formatter tbl sections) values
-  | NodeEmpty         -> Format.pp_print_string formatter "[]"
-and print_table formatter toml_table sections =
-    (*
-     * We need to print non-table values first, otherwise we risk including
-     * top-level values in a section by accident
-     *)
-  let (table_with_table_values, table_with_non_table_values) =
-    TomlMap.partition is_table toml_table
-  in
-  let print_key_value key value =
-    print_value_with_key formatter key value sections
-  in
-  (* iter() guarantees that keys are returned in ascending order *)
-  TomlMap.iter print_key_value table_with_non_table_values;
-  TomlMap.iter print_key_value table_with_table_values
-and print_value formatter toml_value sections =
-  match toml_value with
-  | TBool value   -> print_bool formatter value
-  | TInt value    -> print_int formatter value
-  | TFloat value  -> print_float formatter value
-  | TString value -> print_string formatter value
-  | TDate value   -> print_date formatter value
-  | TArray value  -> print_array formatter value sections
-  | TTable value  -> print_table formatter value sections
-and print_value_with_key formatter key toml_value sections =
-  let sections', add_linebreak = match toml_value with
+  let pp_list printer lst =
+    Format.pp_print_char fmt '[';
+    pp_list fmt (fun fmt () -> Format.pp_print_string fmt ", ") printer lst ;
+    Format.pp_print_char fmt ']' in
+
+  match a with
+  | NodeEmpty       -> Format.pp_print_string fmt "[]"
+  | NodeBool node   -> pp_list pp_bool node
+  | NodeInt node    -> pp_list pp_int node
+  | NodeFloat node  -> pp_list pp_float node
+  | NodeString node -> pp_list pp_string node
+  | NodeDate node   -> pp_list pp_date node
+  | NodeArray node  -> pp_list (fun fmt arr -> pp_array fmt arr sections) node
+  | NodeTable node  ->
+     let is_array_of_table =
+       fun _ -> function TArray (NodeTable _) -> true | _ -> false in
+     List.iter (fun t ->
+                (* Don't print the intermediate sections,
+                 * if all node are arrays of tables,
+                 * print [[x.y.z]] as appropriate instead of [[x]][[y]][[z]] *)
+                if not (M.for_all is_array_of_table t)
+                then Format.fprintf fmt "[[%s]]\n"
+                                    (sections
+                                     |> List.map K.to_string
+                                     |> String.concat ".");
+                pp_table fmt t sections) node
+
+and pp_key fmt k = Format.pp_print_string fmt (K.to_string k)
+
+and pp_nested_keys fmt (ks : K.t list) =
+  pp_list fmt (fun fmt () -> Format.pp_print_char fmt '.') pp_key ks
+
+and pp_table fmt t sections =
+  (* We need to print non-table values first,
+   * otherwise we risk including
+   * top-level values in a section by accident *)
+  let (subtables, toplevel) = M.partition is_table t in
+  let pp_key_value key value = pp_key_value fmt key value sections in
+
+  (* iter () guarantees that keys are returned in ascending order *)
+  M.iter pp_key_value toplevel ;
+  M.iter pp_key_value subtables
+
+and pp_value fmt v path =
+  match v with
+  | TBool value   -> pp_bool fmt value
+  | TInt value    -> pp_int fmt value
+  | TFloat value  -> pp_float fmt value
+  | TString value -> pp_string fmt value
+  | TDate value   -> pp_date fmt value
+  | TArray value  -> pp_array fmt value path
+  | TTable value  -> pp_table fmt value path
+
+and pp_key_value fmt key v path =
+
+  let path', add_linebreak = match v with
     | TTable value  ->
-      let sections_with_key = sections @ [key] in
-      (*
-       * Don't print the intermediate sections, if all values are tables,
-       * print [x.y.z] as appropriate instead of [x][y][z]
-       *)
-      if not (TomlMap.for_all is_table value)
-      then Format.fprintf formatter "[%s]\n"
-          (sections_with_key |> List.map TomlKey.to_string |> String.concat ".");
-      (sections_with_key, false)
-    | TArray (NodeTable tables) ->
-      let sections_with_key = sections @ [key] in
-      (sections_with_key, false)
-    | _             ->
-      Format.fprintf formatter "%s = " (TomlKey.to_string key);
-      (sections, true)
+       (* Don't print the intermediate path, if all values are tables,
+        * print [x.y.z] as appropriate instead of [x][y][z] *)
+       let path = path @ [ key ] in
+       if not (M.for_all is_table value)
+       then Format.fprintf fmt "[%a]\n" pp_nested_keys path ;
+       (path, false)
+    | TArray (NodeTable tables) -> (path @ [key], false)
+    | _ -> Format.fprintf fmt "%s = " (K.to_string key) ;
+           (path, true)
   in
-  print_value formatter toml_value sections';
+  pp_value fmt v path';
   if add_linebreak
-  then Format.pp_print_char formatter '\n'
+  then Format.pp_print_char fmt '\n'
 
-let value formatter toml_value =
-  print_value formatter toml_value [];
-  Format.pp_print_flush formatter ()
+let value fmt v = pp_value fmt v [] ; Format.pp_print_flush fmt ()
 
-let array formatter toml_array =
-  match toml_array with
+let array fmt = function
   | NodeTable t ->
-    (* We need the parent section for printing an array of table correctly,
-     otheriwise the header contains [[]] *)
-    invalid_arg "Cannot format array of tables, use Toml.Printer.table"
-  | _           ->
-    print_array formatter toml_array [];
-    Format.pp_print_flush formatter ()
+     (* We need the parent section for printing an array of table correctly,
+     * otheriwise the header contains [[]] *)
+     invalid_arg "Cannot format array of tables, use Toml.Printer.table"
+  | x -> pp_array fmt x [];
+         Format.pp_print_flush fmt ()
 
-let table formatter toml_table =
-  print_table formatter toml_table [];
-  Format.pp_print_flush formatter ()
+let table fmt t =
+  pp_table fmt t [];
+  Format.pp_print_flush fmt ()
