97c02eaae7d8da2946c95a7e50a9682d9a90b9d6
Stavros Polymenis <sp@orbitalfox.com>
2017-11-07 16:40:14 +0000

    Revise for 4.06.0 immutable Strings

--- src/tomlUnicode.ml
+++ src/tomlUnicode.ml
@@ -10,28 +10,29 @@
 let to_utf8 u =
   let dec = int_of_string @@ "0x" ^ u in
   let update_byte s i mask shift =
-    s.[i] <- Char.chr
-             @@ Char.code s.[i] + dec lsr shift land int_of_string mask in
+    (Char.chr @@ Char.code (Bytes.get s i) + dec lsr shift land int_of_string mask)
+    |> Bytes.set s i
+  in
   if dec > 0xFFFF then
     failwith ("Invalid escaped unicode \\u" ^ u)
   else if dec > 0x7FF then
     begin
-      let s = String.copy "\224\128\128" in
+      let s = Bytes.of_string "\224\128\128" in
       update_byte s 2 "0b00111111" 0;
       update_byte s 1 "0b00111111" 6;
       update_byte s 0 "0b00001111" 12;
-      s
+      Bytes.to_string s
     end
   else if dec > 0x7F then 
     begin
-      let s = String.copy "\192\128" in
+      let s = Bytes.of_string "\192\128" in
       update_byte s 1 "0b00111111" 0;
       update_byte s 0 "0b00011111" 6;
-      s
+      Bytes.to_string s
     end
   else
     begin
-      let s = String.copy "\000" in
+      let s = Bytes.of_string "\000" in
       update_byte s 0 "0b01111111" 0;
-      s
+      Bytes.to_string s
     end
