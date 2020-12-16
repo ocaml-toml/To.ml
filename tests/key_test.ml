9a6c105ebe560ac0acc8bb3e4f28028455c3bbe9
Spiros Eliopoulos <seliopou@gmail.com>
2020-12-16 16:52:53 -0500

    key-compare: fix typo

--- tests/key_test.ml
+++ tests/key_test.ml
@@ -4,7 +4,7 @@ open Utils
 let test_bad_bk k () =
   assert_raises (Toml.Types.Table.Key.Bad_key k) (fun () -> Toml.Min.key k)
 
-let quoted_bare_comparsion () =
+let quoted_bare_comparison () =
   let module Key = Toml.Types.Table.Key in
   let q = Key.quoted_key_of_string in
   let b = Key.bare_key_of_string  in
@@ -18,7 +18,7 @@ let suite =
              (Toml.Types.Table.Key.to_string
                 (Toml.Types.Table.Key.quoted_key_of_string
                    "my_good_unicodé_key")) )
-       ; "quoted vs bare" >:: quoted_bare_comparsion
+       ; "quoted vs bare" >:: quoted_bare_comparison
        ; "key with spaces" >:: test_bad_bk "key with spaces"
        ; "key with tab" >:: test_bad_bk "with\ttab"
        ; "key with linefeed" >:: test_bad_bk "with\nlinefeed"
